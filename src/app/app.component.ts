8927e87cf70ed26569884d30c5f1556027c1bcfd
Oscar Suarez Payo <osuarez@atsistemas.com>
2024-07-31 00:52:30 +0100

    first angular commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { NgOptimizedImage } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, NgOptimizedImage],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title = 'chokismotoclub';
+}
